bae05a47c2fc5a09254c1544e8e8972064fec83d
ryan mckeown <ryan.w.mckeown@gmail.com>
2016-05-27 12:14:16 -0400

    comments

--- bowling/src/main/scala/com/mckeown/bowling/Bowling.scala
+++ bowling/src/main/scala/com/mckeown/bowling/Bowling.scala
@@ -8,9 +8,12 @@ class Bowling {
   def score(frames:List[Any]):Int = {0}
 }
 
+// This is a companion object which will help us not have to keep
+// creating new Bowling() everytime we want to use it
 object BowlingGame {
 
   def apply() = new Bowling()
 
+  // this just allows for writing easier test functions
   def score(frames:List[Any]):Int = new Bowling().score(frames)
 }
