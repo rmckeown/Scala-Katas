c5bb61ef3abe70a0c18947b6b0751511dd6bf8f1
rmckeown <rmckeown@users.noreply.github.com>
2016-09-06 09:46:02 -0400

    Update FizzBuzz.scala

--- fizzbuzz/src/main/scala/com/mckeown/fizzbuzz/FizzBuzz.scala
+++ fizzbuzz/src/main/scala/com/mckeown/fizzbuzz/FizzBuzz.scala
@@ -4,7 +4,7 @@ object FizzBuzz{
   
   def convertToFizzBuzz(input:Int):String = {""}
 
-  // There are 2 apply methods which will us to either calculate FizzBuzz
+  // There are 2 apply methods which will allow us to calculate FizzBuzz
   // for a single integer or for a list of integers
   // FizzBuzz(3) or FizzBuzz(List(1,3,4,5,30))
   def apply(input:Int):String = convertToFizzBuzz(input)
